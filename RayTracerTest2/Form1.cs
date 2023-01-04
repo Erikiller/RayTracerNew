0adf7705cafbd8995b80c768093849db8878441a
Erikiller <Erikiller@users.noreply.github.com>
2023-01-04 13:48:27 +0100

    Not working rn but I will push

--- RayTracerTest2/Form1.cs
+++ RayTracerTest2/Form1.cs
@@ -7,13 +7,17 @@ namespace RayTracerTest2
 {
     public partial class Form1 : Form
     {
+        public string TextOfForm { get; set; }
+
         public Form1()
         {
             InitializeComponent();
             double aspectRatio = 16.0 / 9.0;
             Width = 1200;
             Height = (int)(Width / aspectRatio);
-            
+
+            TextOfForm = "Rendering Program";
+            this.Text = TextOfForm;
         }
 
         /// <summary>
@@ -22,43 +26,48 @@ namespace RayTracerTest2
         /// <returns>Bitmap</returns>
         unsafe Bitmap Render()
         {
+            Random rnd = new();
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
             // Image
-            const float aspectRatio = 16.0f/9.0f;
+            const float aspectRatio = 16.0f / 9.0f;
             const int imageWidth = 1200;
             const int imageHeight = (int)(imageWidth / aspectRatio);
-            
+
             // World
             HittableList world = new();
-            
+
             //Be careful with the order of rendering the objects,
             //there can be some issues with how the objects are placed
-            world.Add(new Sphere(new Vector3(0f,-100.5f,-1f),100f));
-            world.Add(new Sphere(new Vector3(0f,0f,-1f),0.5f));
-            
+            world.Add(new Sphere(new Vector3(0f, -100.5f, -1f), 100f));
+            world.Add(new Sphere(new Vector3(0f, 0f, -1f), 0.5f));
+
             // Camera
             float viewportHeight = 2.0f;
             float viewportWidth = aspectRatio * viewportHeight;
             float focalLength = 1.0f;
-            
+
             Vector3 origin = new Vector3(0, 0, 0);
             Vector3 horizontal = new Vector3(viewportWidth, 0, 0);
             Vector3 vertical = new Vector3(0, viewportHeight, 0);
             Vector3 lowerLeftCorner = origin - horizontal / 2 - vertical / 2 - new Vector3(0, 0, focalLength);
 
+            // Camera
+            Camera cam = new();
+
+            float samplesPerPixel = 10;
             byte bytesPerPixel = 24;
-            
+
             Bitmap img = new(imageWidth, imageHeight, PixelFormat.Format24bppRgb);
 
             BitmapData imgData = img.LockBits(
-                new Rectangle(0, 0, imageWidth, imageHeight), 
-                ImageLockMode.WriteOnly, 
+                new Rectangle(0, 0, imageWidth, imageHeight),
+                ImageLockMode.WriteOnly,
                 img.PixelFormat);
 
             // Scan0 => determine/calls the first address of the bitmap
             byte* imgPointer = (byte*)imgData.Scan0.ToPointer();
-            
+
             for (int x = 0; x < imageWidth; x++)
             {
                 for (int y = 0; y < imageHeight; y++)
@@ -69,7 +78,7 @@ namespace RayTracerTest2
                     // Wenn der Schritt positiv ist, ist die Bitmap oben unten.
                     // Wenn der Schritt negativ ist, ist die Bitmap unten nach oben.
                     byte* data = imgPointer + y * imgData.Stride + x * bytesPerPixel / 8;
-                        
+                    /*    
                     float u = (float)(x) / (imageWidth - 1);
                     float v = (float)(y) / (imageHeight - 1);
                     
@@ -77,27 +86,51 @@ namespace RayTracerTest2
                     Ray r = new Ray(origin, dir);
 
                     Vector3 color = RayColor(r, world);
+                    */
 
-                    color = new Vector3(
+                    Vector3 color = Vector3.Zero;
+                    for (int s = 0; s < samplesPerPixel; s++)
+                    {
+                        float u = (x + (float)rnd.NextDouble()) / (imageWidth - 1);
+                        float v = (y + (float)rnd.NextDouble()) / (imageHeight - 1);
+                        Ray r = cam.GetRay(u, v);
+                        color += RayColor(r, world);
+                    }
+
+                    float scale = 1.0f / samplesPerPixel;
+                    color.X *= scale;
+                    color.Y *= scale;
+                    color.Z *= scale;
+
+                    /*color = new Vector3(
                         255 * (float)Math.Sqrt(color.X),
                         255 * (float)Math.Sqrt(color.Y),
                         255 * (float)Math.Sqrt(color.Z)
                     );
+                    */
+                    Mathematics mth = new();
+                    color = new Vector3(
+                        255f * mth.Clamp(color.X, 0.0f, 0.999f),
+                        255f * mth.Clamp(color.Y, 0.0f, 0.999f),
+                        255f * mth.Clamp(color.Z, 0.0f, 0.999f)
+                    );
+                    
                     data[2] = (byte)color.X;
                     data[1] = (byte)color.Y;
                     data[0] = (byte)color.Z;
                 }
             }
+
             img.UnlockBits(imgData);
             img.RotateFlip(RotateFlipType.Rotate180FlipX);
             stopwatch.Stop();
-            Console.WriteLine($"Rendered in {stopwatch.ElapsedMilliseconds} ms");
+            TextOfForm = $"Rendered in {stopwatch.ElapsedMilliseconds} ms";
             return img;
         }
-        
+
         Vector3 RayColor(Ray r, Hittable world)
         {
-            HitRecord rec = world.hit(r, 0f, float.MaxValue);
+            HitRecord rec = world.hit(r, 0.0001f, float.MaxValue);
             if (rec.didHit) return 0.5f * (rec.normal + new Vector3(1, 1, 1));
 
             Vector3 unitDirection = Vector3.Normalize(r.Direction);
@@ -110,7 +143,18 @@ namespace RayTracerTest2
             Bitmap img = Render();
 
             e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
-            e.Graphics.DrawImage(img, new Rectangle(0, 0, Width, Height), 0, 0, img.Width, img.Height, GraphicsUnit.Pixel);
+            e.Graphics.DrawImage(img, new Rectangle(0, 0, Width, Height), 0, 0, img.Width, img.Height,
+                GraphicsUnit.Pixel);
+        }
+    }
+
+    public class Mathematics
+    {
+        public float Clamp(float x, float min, float max)
+        {
+            if (x < min) return min;
+            if (x > max) return max;
+            return x;
         }
     }
 }
