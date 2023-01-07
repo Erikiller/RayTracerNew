77ef32c018b6503c54bb49b2b83897aea3ebd338
Erikiller <Erikiller@users.noreply.github.com>
2023-01-07 18:40:52 +0100

    Updated Materials.cs, do not work rn

--- RayTracerTest2/Materials.cs
+++ RayTracerTest2/Materials.cs
@@ -1,10 +1,10 @@
 ﻿using System.Numerics;
 
 namespace RayTracerTest2;
-
+/*
 public abstract class Materials
 {
-    public Vector3 Albedo;
+    public Vector3 Albedo { get; set; }
 
     public virtual void Lambertian(Vector3 a)
     {
@@ -24,10 +24,53 @@ public abstract class Materials
         return true;
     }
 }
+*/
+
+public abstract class Materials
+{
+    public abstract Scattered Scatter(Ray r, HitRecord rec);
+}
+public struct Scattered
+{
+    public Vector3 Attenuation { get; set; }
+    public Ray ScatteredRay { get; set; }
+    public bool DidScatter { get; set; }
+    public Scattered(Vector3 _attenuation, Ray _scatteredRay, bool _didScatter)
+    {
+        Attenuation = _attenuation;
+        ScatteredRay = _scatteredRay;
+        DidScatter = _didScatter;
+    }
+}
+
+public class Lambertian : Materials
+{
+    public Vector3 Color;
+
+    Lambertian(Vector3 color)
+    {
+        Color = color;
+    }
+    public override Scattered Scatter(Ray r, HitRecord rec)
+    {
+        Scattered scattered = new();
+        Vector3 scatterDir = rec.normal + Mathematics.RandomUnitVector(); // Could possibly be a bug
+        
+        if (Mathematics.NearZero(scatterDir))
+            scatterDir = rec.normal;
+        
+        scattered.ScatteredRay = new Ray(rec.p, scatterDir);
+        scattered.Attenuation = Color;
+        scattered.DidScatter = true;
+        return scattered;
+    }
+}
+/*
 
 public abstract class Metal : Materials
 {
-    Metal(Vector3 a) { Albedo = a; }
+    
+    protected Metal(Vector3 a) { Albedo = a; }
 
     public override bool Scatter(Ray rIn, HitRecord rec, Vector3 _attenuation, Ray _scattered)
     {
@@ -37,3 +80,4 @@ public abstract class Metal : Materials
         return (Vector3.Dot(_scattered.Direction, rec.normal) > 0);
     }
 }
+*/
